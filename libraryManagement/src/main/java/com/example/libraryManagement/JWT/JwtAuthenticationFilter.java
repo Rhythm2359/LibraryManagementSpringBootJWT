22f77347d852477121796f59cfb18ddffb5cd11f
Rhythm2359 <rhythmbhandari44@gmail.com>
2025-11-09 23:27:45 +0530

    updated

---
+++ libraryManagement/src/main/java/com/example/libraryManagement/JWT/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.example.libraryManagement.JWT;
+
+public class JwtAuthenticationFilter {
+}
--- libraryManagement/src/main/java/com/example/libraryManagement/entity/User.java
+++ libraryManagement/src/main/java/com/example/libraryManagement/entity/User.java
@@ -2,22 +2,64 @@ package com.example.libraryManagement.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
-@Table(name = "users")
 @Data
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id ;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String name ;
-    private String email ;
-    private String password ;
+    private String name;
+    private String email;
+    private String password;
 
     @ElementCollection(fetch = FetchType.EAGER)
-    private Set<String> roles ;
+    private Set<String> roles;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
